1d2da54b495f516e9632b92ab06d10d657c1c9d2
monstermichl <vouillarmet.michel@gmail.com>
2024-10-31 20:50:32 +0100

    Update dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         ],
         extras_require={
             'dev': [
-                'ninja-bear>=0.1.1',
+                'ninja-bear>=0.1.2',
                 'wheel>=0.41.1',
                 'twine>=4.0.2',
                 'ruff>=0.0.47',
